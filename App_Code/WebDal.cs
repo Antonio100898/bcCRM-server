eb0bf80722285db4f831527bf4d1a765c917daad
Anton <anymy9813@gmail.com>
2024-10-14 11:16:07 +0300

    fixed updateShiftPlan timezone

--- App_Code/WebDal.cs
+++ App_Code/WebDal.cs
@@ -1962,6 +1962,9 @@ public static class WebDal
 
     internal static void UpdateShiftPlan(ShiftPlan shiftPlan)
     {
+        var remoteTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Israel Standard Time");
+        shiftPlan.startDate = TimeZoneInfo.ConvertTime(shiftPlan.startDate, remoteTimeZone);
+
         if (string.IsNullOrEmpty(shiftPlan.remark))
         {
             shiftPlan.remark = " ";
